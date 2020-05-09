c7fc8c1cd4b9ebce976b6b2449aad8a3a5d411e4
DmitriyKulikovskiy <59229226+DmitriyKulikovskiy@users.noreply.github.com>
2020-05-09 19:32:24 +0300

    Updates

---
+++ precache-manifest.a20f32ed59cb66fbdc4a9420f40c5e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9943c726797d2854492040dabc2446c",
+    "url": "/MyWebShop/index.html"
+  },
+  {
+    "revision": "42dd8cbe76d31187f59f",
+    "url": "/MyWebShop/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0e3db905ff2abf91484c",
+    "url": "/MyWebShop/static/css/main.b2ef10be.chunk.css"
+  },
+  {
+    "revision": "42dd8cbe76d31187f59f",
+    "url": "/MyWebShop/static/js/2.e639f554.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/MyWebShop/static/js/2.e639f554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3db905ff2abf91484c",
+    "url": "/MyWebShop/static/js/main.7097658a.chunk.js"
+  },
+  {
+    "revision": "223a7547d334f7dc6cd1",
+    "url": "/MyWebShop/static/js/runtime-main.77f7faf0.js"
+  }
+]);
